116c552b5cf9812a0615e8b2ff513f92ab3db76d
thaoduong1702 <125199309+thaoduong1702@users.noreply.github.com>
2023-02-15 11:03:04 +0700

    Add files via upload

---
+++ bàiTH3.sql
@@ -0,0 +1,101 @@
+﻿create database MarkManagement
+go
+use MarkManagement
+Create table Students
+(
+StudentID nvarchar(12) primary key,
+StudentName nvarchar(25) not null,
+DateofBirth datetime not null,
+Email nvarchar(40),
+Phone nvarchar(12),
+Class nvarchar(10)
+)
+go
+
+Create table Subjects
+(
+SubjectID nvarchar(10) primary key,
+SubjectName nvarchar(25) not null
+)
+go
+
+create table Mark
+(
+StudentID nvarchar(12),
+SubjectID nvarchar(10),
+Date Datetime,
+Theory Tinyint,
+Practical Tinyint
+PRIMARY KEY(StudentID, SubjectID)
+)
+go
+
+INSERT INTO Students(StudentID, StudentName, DateofBirth, Email, Phone, Class) VALUES ('AV0807005', N'Mai Trung Hiếu', '11-10-1989','trunghieu@yahoo.com',0904115116,'AV1'),
+                                                                                      ('AV0807006',N'Nguyễn Quý Hùng', '02-12-1988', 'quyhung@yahoo.com', 0955667787, 'AV2'),
+																					  ('AV0807007', N'Đỗ Đắc Huỳnh', '02-01-1990', 'dachuynh@yahoo.com', 0988574747,'AV2'),
+																					  ('AV0807009', N'An Đăng Khuê', '06-03-1986', 'dangkhue@yahoo.com', 0986757463, 'AV1'),
+																					  ('AV0807010',N'Nguyễn T. Tuyết Lan','12-07-1989' ,'tuyetlan@yahoo.com', 0983310342,'AV2'),
+																					  ('AV0807011',N'Đinh Phụng Long','02-12-1990','phunglong@yahoo.com', NULL ,'AV1'),
+																					  ('AV0807012',N'Nguyễn Tuấn Nam','02-03-1990','tuannam@yahoo.com', NULL , 'AV1')
+
+INSERT INTO Subjects(SubjectID, SubjectName) VALUES ('S001','SQL'),
+                                                    ('S002','JAVA SIMPLEFIELD'),
+													('S003','ACTIVE SERVER PAGE')
+
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807005', 'S001', 8, 25, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES('AV0807006', 'S002', 16, 30, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807007', 'S001', 10, 25, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807009', 'S003', 7, 13, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807010', 'S003', 9, 16, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807011', 'S002', 8, 30, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807012', 'S001', 7, 31, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807005', 'S002', 12, 11, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807009', 'S003', 11, 20, '06-05-2008')
+INSERT INTO Mark(StudentID, SubjectID, Theory, Practical, Date) VALUES ('AV0807010', 'S001', 7, 6, '06-06-2008')
+
+---Câu 1---
+select * from Students
+go
+---Câu 2---
+select * from Students
+where Class = 'AV1'
+go
+
+---Câu 3---
+UPDATE Students set Class = 'AV2' where StudentID = 'AV0807012'
+
+---Câu 4--
+
+SELECT COUNT(StudentName) as Lớp1
+FROM Students
+WHERE Class = 'AV1';
+
+SELECT COUNT(StudentName) as Lớp2
+FROM Students
+WHERE Class = 'AV2';
+
+---Câu 5--
+Select * From Students Where Class='AV2' Order by StudentName
+
+----Câu 6---
+select * from Students inner join Mark ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10 and Date = '6/5/2008'
+
+----Câu 7----
+select count(Class) from Students inner join Mark ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10
+
+---Câu 8---
+select * from Students 
+where Class = 'AV1' and DateofBirth > '01-01-1980'
+
+---Câu 9---
+DELETE FROM Students
+Where StudentID = 'AV0807011'
+
+---Câu 10---
+SELECT Students.StudentID, StudentName, SubjectName, Theory, Practical, Date 
+FROM Mark 
+INNER JOIN Subjects ON Mark.SubjectID = Subjects.SubjectID 
+INNER JOIN Students ON Mark.StudentID = Students.StudentID 
+WHERE Subjects.SubjectID = 'S001' AND Date = '6/5/2008';
---
+++ sales.sql
@@ -0,0 +1,92 @@
+﻿Exec sp_addtype 'Mota', 'nvarchar(40)','not null';
+Exec sp_addtype 'IDKH', 'char(10)';
+Exec sp_addtype 'DT', 'char(12)','not null';
+
+create database Sales
+go
+use Sales
+Create table SanPham
+(
+MaSp char(6) primary key,
+TenSp varchar(20) not null,
+NgayNhap date,
+DVT char(10) not null,
+SoLuongTon int not null,
+DonGiaNhap money not null
+)
+go
+
+Create table HoaDon
+(
+MaHD char(10) primary key,
+NgayLap date not null,
+NgayGiao date not null,
+MaKH IDKH,
+DienGiai Mota 
+)
+go
+
+Create table KhachHang
+(
+MaKH IDKH primary key,
+TenKH nvarchar(30) not null,
+DiaChi nvarchar(40) not null,
+DienThoai DT 
+)
+go
+
+Create table ChiTietHD
+(
+MaHD char(10) primary key,
+MaSp char(6) not null,
+SoLuong int not null,
+)
+go
+
+---Câu 3------
+ALTER TABLE HoaDon
+ALTER COLUMN DienGiai nvarchar(100)
+
+-----Câu 4----
+ALTER TABLE SanPham
+ADD TyLeHoaHong float
+
+---Câu 5---
+ALTER TABLE SanPham
+DROP COLUMN NgayNhap;
+
+---Câu 6 Tạo khóa phụ-------
+ALTER TABLE ChiTietHD ADD CONSTRAINT fk_MaSp FOREIGN KEY(MaSp) REFERENCES SanPham(MaSp)
+GO
+
+ALTER TABLE HoaDon ADD CONSTRAINT fk_MaKH FOREIGN KEY(MaKH) REFERENCES KhachHang(MaKH)
+GO
+
+----Câu 7
+ALTER TABLE HoaDon
+ADD CONSTRAINT HoaDon_NgayGiao_NgayLap
+Check(NgayGiao >= NgayLap)
+
+ALTER TABLE HoaDon
+ADD CHECK (MaHD like'[A-Z][A-Z][0-9][0-9][0-9][0-9]')
+
+ALTER TABLE HoaDon
+ADD CONSTRAINT HoaDon_NgayLap_D
+Default getdate() for NgayLap
+
+----Câu 8---
+ALTER TABLE SanPham
+ADD CHECK (SoLuongTon > 0 and SoLuongTon < 50)
+
+ALTER TABLE SanPham
+ADD CHECK (DonGiaNhap > 0)
+
+ALTER TABLE SanPham ADD NgayNhap date
+
+ALTER TABLE SanPham
+ADD CONSTRAINT SanPham_NgayNhap_D 
+default  getdate() for NgayNhap
+
+ALTER TABLE SanPham
+ADD CHECK (DVT like 'KG"Thùng"Hộp"Cái')
+
